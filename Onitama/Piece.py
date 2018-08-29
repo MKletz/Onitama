91ef2f01b702c6f42a518468e6d1b6c24e44006a
MKletz <michael.kletz.27@gmail.com>
2018-08-28 21:34:13 -0500

    added elimation placeholder logic

--- Onitama/Piece.py
+++ Onitama/Piece.py
@@ -3,3 +3,9 @@ class Piece():
     def __init__(self):
         self.is_master = False
         self.color = None
+
+    def eliminate(self):
+        if self.is_master:
+            print("you're dead Jim.")
+        else:
+            print("I'm dead Jim.")
